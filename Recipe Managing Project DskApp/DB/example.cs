a42e34ad4fc096728a7b2ba12d380b93db13570e
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-11-15 21:24:13 -0600

    support for multiple recipes

--- Recipe Managing Project DskApp/DB/example.cs
+++ Recipe Managing Project DskApp/DB/example.cs
@@ -1,6 +1,9 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 using System.Xml;
+using static Recipe_Managing_Project_DskApp.DB.recipe;
 using Recipe_Managing_Project_DskApp.DB;
+
 // See https://aka.ms/new-console-template for more information
 
 
@@ -18,8 +21,15 @@ public class example
       //the default execution path is in debug 
       string path = System.Environment.CurrentDirectory.Replace("bin\\Debug", "\\DB\\dataFile.xml");
         read.read(path);
-      // this writes the same entry 
-        write.write(path,read.getRecipe());
+        List<recipe.Recipe> recipes = read.getRecipe();
+        recipe.Name n = new  recipe.Name("Water", "easy");
+        recipe.Restrictions r = new Restrictions("False","False");
+        recipe.Ingredient i = new Ingredient("water","1","cup");
+        List< recipe.Ingredient> il = new List<recipe.Ingredient>();
+        il.Add(i);
+        recipe.Recipe rep = new recipe.Recipe(n,r,il);
+
+        write.write(path, rep);
 
 
     }
--- Recipe Managing Project DskApp/DB/fileRead.cs
+++ Recipe Managing Project DskApp/DB/fileRead.cs
@@ -12,7 +12,7 @@ namespace Recipe_Managing_Project_DskApp.DB
     internal class fileRead
     {
         XmlDocument xmlDoc;
-        public Recipe recipe;
+        public List<Recipe> recipe;
 
         public fileRead(XmlDocument doc)
         {
@@ -22,34 +22,43 @@ namespace Recipe_Managing_Project_DskApp.DB
 
         public void read(string path)
         {
-             xmlDoc.Load(path);
+            xmlDoc.Load(path);
             var element = xmlDoc.DocumentElement;
-            var root = element.ChildNodes[0];
+            var root = element.ChildNodes;
+            recipe = new List<Recipe>();
             List<Ingredient> ingredients = new List<Ingredient>();
             Name name = new Name();
             Restrictions restrictions = new Restrictions();
-
-            foreach (XmlNode node in root.ChildNodes)
-
+            for (int i = 0; i < root.Count; i++)
             {
-                if (node.Name == "name")
-                {
-                    name = new Name(node.InnerText, node.Attributes[0].InnerText);
-                }
-                else if (node.Name == "restrictions")
-                {
-                    restrictions = new Restrictions(node.Attributes[0].InnerText, node.Attributes[1].InnerText);
-                }
-                else if (node.Name == "ingredient")
-                {
-                    ingredients.Add(new Ingredient(node.Attributes[0].InnerText, node.Attributes[1].InnerText, node.Attributes[2].InnerText));
+                foreach(XmlNode node in root[i].ChildNodes) {
+  
+                    if (node.Name == "name")
+                    {
+                        name = new Name(node.InnerText, node.Attributes[0].InnerText);
+                    }
+                    else if (node.Name == "restrictions")
+                    {
+                        restrictions = new Restrictions(node.Attributes[0].InnerText, node.Attributes[1].InnerText);
+                    }
+                    else if (node.Name == "ingredient")
+                    {
+                        ingredients.Add(new Ingredient(node.Attributes[0].InnerText, node.Attributes[1].InnerText, node.Attributes[2].InnerText));
+
+                    }
 
                 }
+            
+                recipe.Add(new Recipe(name, restrictions, ingredients));
+                name = new Name();
+                restrictions = new Restrictions();
+                ingredients = new List<Ingredient>();
             }
-            recipe = new Recipe(name, restrictions, ingredients);
         }
+            
+        
     
-        public Recipe getRecipe()
+        public List<Recipe> getRecipe()
         {
             return recipe;
         }
